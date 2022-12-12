4f12ffe24c5146fd9bb7103da71803715c932f50
ThatOneRuffian <MarcusMesta@protonmail.com>
2022-12-12 11:48:33 -0800

    Initial commit
    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+'''This is a test module to demonstrate the git process'''
+
+
+def main() -> None:
+    print('Hello World!')
+
+
+if __name__ == "__main__":
+    main()
+else;
+    print('Not main module, exiting')
